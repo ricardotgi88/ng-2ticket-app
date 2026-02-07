e05e8dd3090fc57db714925938999768c1542880
Ricardo Henrique dos Santos <ricardotgi88@hotmail.com>
2026-02-07 17:14:58 +0000

    feat: improve cart interface

--- src/app/core/interfaces/cart.interface.ts
+++ src/app/core/interfaces/cart.interface.ts
@@ -1,7 +1,29 @@
-import { CartTicket } from './cart-ticket.interface';
+import { Event } from '../../api/models/event.interface';
+import { TicketTypeEnum } from '../enums/ticket-type.enum';
+
+export interface CartTicket {
+  id: number;
+  name: string;
+  ticketType: TicketTypeEnum;
+  amount: number;
+  price: number;
+  total: number;
+}
+
+export interface CartTax {
+  name: string;
+  value: number;
+}
+
+export interface CartEvent {
+  event: Event;
+  tickets: CartTicket[];
+}
 
 export interface Cart {
+  events: CartEvent[];
+  servicesFee: number;
+  taxes: CartTax[];
   totalPrice: number;
   totalTickets: number;
-  tickets: CartTicket[];
 }
